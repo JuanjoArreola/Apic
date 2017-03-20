ad1cd43a5d30781c0847faa67a9e6ba775a2fb27
Juanjo <juanjo.arreola@cinemasapp.co>
2017-03-20 16:01:40 -0600

    Fix use CGFloat in Color extension

--- Sources/AbstractModel.swift
+++ Sources/AbstractModel.swift
@@ -8,10 +8,8 @@
 
 #if os(OSX)
     import AppKit
-    public typealias Color = NSColor
 #else
     import UIKit
-    public typealias Color = UIColor
 #endif
 
 
--- Sources/Color+Hex.swift
+++ Sources/Color+Hex.swift
@@ -6,7 +6,13 @@
 //
 //
 
-import Foundation
+#if os(OSX)
+    import AppKit
+    public typealias Color = NSColor
+#else
+    import UIKit
+    public typealias Color = UIColor
+#endif
 
 extension Color {
     convenience init?(hex: String) {
