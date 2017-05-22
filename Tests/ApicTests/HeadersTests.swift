ef9b759ea72218037fdeea860a982a7b1cee61bd
Juanjo <juanjo.arreola@gmail.com>
2017-05-22 10:46:13 -0500

    Fix tests

--- Tests/ApicTests/HeadersTests.swift
+++ Tests/ApicTests/HeadersTests.swift
@@ -8,6 +8,7 @@
 
 import XCTest
 import Apic
+import AsyncRequest
 
 class HeadersTests: XCTestCase {
     
--- Tests/ApicTests/RepositoryStatusTests.swift
+++ Tests/ApicTests/RepositoryStatusTests.swift
@@ -8,6 +8,7 @@
 
 import XCTest
 import Apic
+import AsyncRequest
 
 class RepositoryStatusTests: XCTestCase {
     
