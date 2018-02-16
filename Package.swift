42c5ae9832d3a20eb988d4d6596b4e152ab4e516
Juanjo <juanjo.arreola@gmail.com>
2018-02-15 18:24:38 -0600

    Package updated

--- Package.swift
+++ Package.swift
@@ -1,8 +1,27 @@
+// swift-tools-version:4.0
+
 import PackageDescription
 
 let package = Package(
     name: "Apic",
+    products: [
+        .library(
+            name: "Apic",
+            targets: ["Apic"]),
+        ],
     dependencies: [
-        .Package(url: "https://github.com/JuanjoArreola/AsyncRequest.git", majorVersion: 2)
+        .package(url: "https://github.com/JuanjoArreola/AsyncRequest.git", from: "2.1.0")
+    ],
+    targets: [
+        .target(
+            name: "Apic",
+            dependencies: ["AsyncRequest"],
+            path: "Sources"
+        ),
+        .testTarget(
+            name: "ApicTests",
+            dependencies: ["Apic"],
+            path: "Tests"
+        )
     ]
 )
